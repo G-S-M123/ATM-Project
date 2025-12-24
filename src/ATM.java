2db95fe8e7e61cd7c2bead674135601dc14a131c
unknown <abclibrarymanagement@gmail.com>
2025-12-24 15:40:47 +0530

    update input buffer error

--- src/ATM.java
+++ src/ATM.java
@@ -1,61 +1,107 @@
 
 import java.util.*;
 
-class Account{
+class Account {
+
     private int CN;
     private int PN;
-    
-    void setCustomerNumber(int cn){ // setter
+
+    void setCustomerNumber(int cn) { // setter
         CN = cn;
     }
 
-    void setPINNumber(int pn){ // setter
+    void setPINNumber(int pn) { // setter
         PN = pn;
     }
 
-    int getCustomerNumber(){ //getter
+    int getCustomerNumber() { //getter
         return CN;
     }
 
-    int getPINNumber(){ // getter
+    int getPINNumber() { // getter
         return PN;
     }
 }
-class OptionMenu extends Account{
+
+class OptionMenu extends Account {
+
     Scanner sc = new Scanner(System.in);
     HashMap<Integer, Integer> data = new HashMap<>();
 
-    void getLogin(){
+    void getLogin() {
+        do {
+            try {
+                data.put(11111, 111);
+                data.put(11112, 222);
+                data.put(11113, 333);
+                data.put(11114, 444);
+                data.put(11115, 111);
 
-        data.put(11111, 111);
-        data.put(11112, 222);
-        data.put(11113, 333);
-        data.put(11114, 444);
-        data.put(11115, 111);
+                System.out.println("Welcome to the RCOEM ATM");
 
-        System.out.println("Welcome to the RCOEM ATM");
+                System.out.print("\nEnter Custormer Number  : ");
+                setCustomerNumber(sc.nextInt());
 
-        System.out.print("\nEnter Custormer Number  : ");
-        setCustomerNumber(sc.nextInt());
+                System.out.print("Enter PIN Number        : ");
+                setPINNumber(sc.nextInt());
 
-        System.out.print("Enter Pin Number        : ");
-        setPINNumber(sc.nextInt());
+                int P = getCustomerNumber();
+                int Q = getPINNumber();
 
-        int P = getCustomerNumber();
-        int Q = getPINNumber();
+                if (data.containsKey(P) && data.get(P) == Q) {
+                    getAccountType();
+                } else {
+                    System.err.println("\nWrong Customer Number or PIN Number");
+                    System.out.println("Enter the VALID Customer Number or PIN Number \n");
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("\nEnter Only Numbers.");
+                System.out.println("Characters and Symbols not allowed.\n");
+                sc.next();
+            }
+        } while (true);
 
-        if (data.containsKey(P) && data.get(P) == Q) {
-            System.out.println("login successful");
+    }
+
+    void getAccountType() {
+        System.out.println("\nACCOUNT TYPE");
+        System.out.println("\nType 1: Current Account");
+        System.out.println("Type 2: Saving Account");
+        System.out.println("Type 3: Exit");
+        System.out.print("\nChoice :");
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                getCurrent();
+                break;
+            case 2:
+                getSaving();
+                break;
+            case 3:
+                System.out.println("\nThank you for VISITING");
+                System.out.println("VISIT AGAIN\n");
+                break;
+            default:
+                System.err.println("\nInvalid Choice");
+                System.out.println("Enter the VALID CHOICE");
+                getAccountType(); // Recursion
+                break;
         }
-        else{
-            System.out.println("wrong");
-        }
+    }
+
+    void getCurrent(){
+
+    }
+
+    void getSaving(){
+        
     }
 }
 
+public class ATM extends OptionMenu {
 
-
-public class ATM extends OptionMenu{
     public static void main(String[] args) throws Exception {
         OptionMenu op = new OptionMenu();
         op.getLogin();
