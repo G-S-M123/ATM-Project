1d635ab2a080320259584aa89b0b15842be9ad17
unknown <abclibrarymanagement@gmail.com>
2025-12-24 14:13:09 +0530

    update account number and pin number getter and setter

--- src/ATM.java
+++ src/ATM.java
@@ -1,18 +1,37 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 class Account{
-    int CN;
+    private int CN;
+    private int PN;
+    
     void setCustomerNumber(int cn){
         CN = cn;
+    }
+
+    void setPinNumber(int pn){
+        PN = pn;
     }
 }
 class OptionMenu extends Account{
     Scanner sc = new Scanner(System.in);
+    HashMap<Integer, Integer> data = new HashMap<>();
+
     void getLogin(){
+
+        data.put(11111, 111);
+        data.put(11112, 222);
+        data.put(11113, 333);
+        data.put(11114, 444);
+        data.put(11115, 111);
+        
         System.out.println("Welcome to the RCOEM ATM");
-        System.out.print("\nEnter Custormer Number: ");
+
+        System.out.print("\nEnter Custormer Number  : ");
         setCustomerNumber(sc.nextInt());
+
+        System.out.print("Enter Pin Number        : ");
+        setPinNumber(sc.nextInt());
     }
 }
 
@@ -20,6 +39,7 @@ class OptionMenu extends Account{
 
 public class ATM extends OptionMenu{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        OptionMenu op = new OptionMenu();
+        op.getLogin();
     }
 }
