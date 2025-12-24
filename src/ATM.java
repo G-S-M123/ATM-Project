7d198feb63f971bac9ea6e356af316d5abe075a9
unknown <abclibrarymanagement@gmail.com>
2025-12-24 14:34:43 +0530

    new commit

--- src/ATM.java
+++ src/ATM.java
@@ -5,12 +5,20 @@ class Account{
     private int CN;
     private int PN;
     
-    void setCustomerNumber(int cn){
+    void setCustomerNumber(int cn){ // setter
         CN = cn;
     }
 
-    void setPinNumber(int pn){
+    void setPINNumber(int pn){ // setter
         PN = pn;
+    }
+
+    int getCustomerNumber(){ //getter
+        return CN;
+    }
+
+    int getPINNumber(){ // getter
+        return PN;
     }
 }
 class OptionMenu extends Account{
@@ -24,14 +32,24 @@ class OptionMenu extends Account{
         data.put(11113, 333);
         data.put(11114, 444);
         data.put(11115, 111);
-        
+
         System.out.println("Welcome to the RCOEM ATM");
 
         System.out.print("\nEnter Custormer Number  : ");
         setCustomerNumber(sc.nextInt());
 
         System.out.print("Enter Pin Number        : ");
-        setPinNumber(sc.nextInt());
+        setPINNumber(sc.nextInt());
+
+        int P = getCustomerNumber();
+        int Q = getPINNumber();
+
+        if (data.containsKey(P) && data.get(P) == Q) {
+            System.out.println("login successful");
+        }
+        else{
+            System.out.println("wrong");
+        }
     }
 }
 
